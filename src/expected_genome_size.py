3d768ac2d1acd87cfc5855f3e8bb52e4db5deedb
masikol <maximdeynonih@gmail.com>
2021-03-22 20:53:10 +0300

    complete egs but test fails

--- src/expected_genome_size.py
+++ src/expected_genome_size.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from typing import NewType, Tuple
+from typing import NewType, Tuple, Sequence, ValuesView
 
 from src.contigs import ContigIndex, ContigCollection
 from src.overlaps import Overlap, OverlapCollection
@@ -22,6 +22,9 @@ from src.combinator_statistics import calc_sum_contig_lengths
 
 
 OverlapCoords = NewType('OverlapCoords', Tuple[int, int])
+
+COORDS = 0
+OVL_LEN = 1
 
 
 class MatchMatrix:
@@ -46,6 +49,32 @@ class MatchMatrix:
     def __repr__(self) -> str:
         return str(self._matrix)
     # end def __repr__
+
+    def __delitem__(self, key):
+        del self._matrix[key]
+    # end def __delitem__
+
+    def get_overlaps_by_index(self, index: ContigIndex) -> Sequence[Overlap]:
+        return tuple(
+            filter(
+                lambda x: x[COORDS][0] == index,
+                self._matrix.items()
+            )
+        )
+    # end def get_overlaps_by_key
+
+    def get_keys_by_index(self, index: ContigIndex) -> Sequence[Overlap]:
+        return tuple(
+            filter(
+                lambda x: x[0] == index,
+                self._matrix.keys()
+            )
+        )
+    # end def get_overlaps_by_key
+
+    def items(self) -> ValuesView:
+        return self._matrix.items()
+    # end def values
 # end class MatchMatrix
 
 
@@ -108,12 +137,79 @@ def _fill_matrices(overlap_collection: OverlapCollection) -> Tuple[MatchMatrix, 
     return SmE, EmS, SmS, EmE
 # end def _fill_matrices
 
+
 def _trace_back(
     matrices: Tuple[MatchMatrix, MatchMatrix, MatchMatrix, MatchMatrix],
     contig_collection: ContigCollection
 ) -> int:
 
     expexted_genome_size: int = calc_sum_contig_lengths(contig_collection)
+    SmE, EmS, SmS, EmE = matrices
 
+    # For comments for loop below, let x denote to start of a contig and y -- to end of a contig.
+    # On the second iteration it will be vice versa: x will be end of a contig and y -- start.
+    # But for clarity, in comments we will write explicitly: "start" for x and "end" for y.
+
+    XmY: MatchMatrix
+    YmX: MatchMatrix
+    XmX: MatchMatrix
+    for XmY, YmX, XmX in zip((SmE, EmS), (EmS, SmE), (SmS, EmE)):
+        i: ContigIndex
+        for i in range(len(contig_collection)):
+
+            max_overlap = _get_largest_ovl(i, XmY, XmX)
+
+            if max_overlap[OVL_LEN] != 0:
+                # Find number of contig, to which current start is adjasent
+                mate_idx = max_overlap[COORDS][1]
+                # Amend genome length -- substract length of the overlap
+                #   multiplied by minimum of multiplicity of adjacent contigs
+                expexted_genome_size -= max_overlap[OVL_LEN]\
+                                        * min(
+                                              contig_collection[i].multplty,
+                                              contig_collection[mate_idx].multplty
+                                          )
+
+                _clear_matrices(i, mate_idx, XmY, YmX, XmX)
+            # end if
+        # end for
+    # end for
+
+    return int(expexted_genome_size)
 
 # end def _trace_back
+
+
+def _get_largest_ovl(i, XmY, XmX):
+    XmY_ovls = XmY.get_overlaps_by_index(i)
+    XmX_ovls = XmX.get_overlaps_by_index(i)
+    if len(XmY_ovls) != 0 or len(XmX_ovls) != 0:
+        max_overlap = max(XmY_ovls + XmX_ovls,
+                          key=lambda x: x[1]
+                      )
+    else:
+        max_overlap = ((None, None), 0)
+    # end if
+    return max_overlap
+# end def _get_largest_ovl
+
+
+def _clear_matrices(i, mate_idx, XmY, YmX, XmX):
+
+    # Set all elements in XmY, YmX and XmX, which overlaps with current start.
+    # I.e. not only maximum one -- all.
+    # XmY[j][i] has nothing to do with current start -- it should be left >0, if it is.
+    # That is why we actually need XmX matrices.
+    for coords in XmY.get_keys_by_index(i):
+        del XmY[coords]
+    # end for
+
+    for matrix in (YmX, XmX):
+        for coords in matrix.get_keys_by_index(i):
+            del matrix[coords]
+        # end for
+        for coords in matrix.get_keys_by_index(mate_idx):
+            del matrix[coords]
+        # end for
+    # end for
+# end def _clear_matrices
--- tests/test_expected_genome_size.py
+++ tests/test_expected_genome_size.py
@@ -8,21 +8,26 @@ from tests.mock_contigs import mock_contigs
 
 
 @pytest.fixture
-def mock_martix():
+def mock_matrix():
     matrix = egs.MatchMatrix()
     matrix[(1, 2)] = 3
     return matrix
-# end def mock_martix
+# end def mock_matrix
 
 class TestMatchMatrix:
 
-    def test_getitem(self, mock_martix):
-        assert mock_martix[(1, 2)] == 3
+    def test_getitem(self, mock_matrix):
+        assert mock_matrix[(1, 2)] == 3
     # end def test_additem_getitem
 
-    def test_getitem_zero(self, mock_martix):
-        assert mock_martix[(2, 3)] == 0
+    def test_getitem_zero(self, mock_matrix):
+        assert mock_matrix[(2, 3)] == 0
     # end def test_additem_getitem
+
+    def test_get_overlaps_by_index(self, mock_matrix):
+        mock_matrix[(1, 3)] = 4
+        assert mock_matrix.get_overlaps_by_index(1) == (((1, 2), 3), ((1, 3), 4))
+    # end def test_get_overlaps_by_index
 # end class TestMatchMatrix
 
 
@@ -35,13 +40,19 @@ def mock_matrices(mock_contigs):
 
 class TestExpectedGenomeSize:
 
-    def test_matrix(self, mock_matrices):
+    def test_fill_matrices(self, mock_matrices):
 
         assert repr(mock_matrices[0]) == '{(0, 1): 12, (1, 0): 13, (4, 6): 8}'
         assert repr(mock_matrices[1]) == '{(0, 1): 13, (1, 0): 12, (6, 4): 8}'
         assert repr(mock_matrices[2]) == '{(4, 5): 14, (5, 4): 14}'
         assert repr(mock_matrices[3]) == '{(2, 3): 17, (3, 2): 17}'
-    # end def test_matrix
+    # end def test_fill_matrices
+
+    def test_traceback(self, mock_contigs):
+        contig_collection, overlap_collection = mock_contigs
+        matrices = egs._fill_matrices(overlap_collection)
+        assert egs._trace_back(matrices, contig_collection) == 414
+    # end def
 
 
 # end class TestExpectedGenomeSize
